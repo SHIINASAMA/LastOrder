4a77a52c30598ef2b94c4c4909a05a01c24183b9
SHIINASAMA <18877410556@163.com>
2020-02-16 12:52:37 +0800

    试用Spire.Doc完成最基础的读取功能，效果不理想

---
+++ TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spire.Doc;
+using Spire.Doc.Documents;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document doc = new Document();
+            doc.LoadFromFile(@"C:\Users\Administrator\source\repos\LastOrder\语文作文.docx");
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach(Section section in doc.Sections) 
+            {
+                foreach(Paragraph paragraph in section.Paragraphs) 
+                {
+                    stringBuilder.Append(paragraph.Text + "\n");
+                }
+            }
+            Console.WriteLine(stringBuilder);
+            Console.ReadLine();
+        }
+    }
+}
